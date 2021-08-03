8051a714497592dc9cc6caedc9e98fccfa2d3dcf
ngodinhtri <ngodinhtri@outlook.com>
2021-08-03 16:54:17 +0700

    code again, using bootstrap

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+//expand previous weather
+// document.querySelector("#expand-arrow").onchange = function (e) {
+//   const previousWeatherElement = document.querySelector("#previous-weather");
+//   previousWeatherElement.classList.toggle("show-weather");
+// };
