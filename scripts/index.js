b766b2d88deb5f81ec20fd784cebe9b53f6ce193
ngodinhtri <ngodinhtri@outlook.com>
2021-08-05 14:48:41 +0700

    Complete UI

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-//expand previous weather
-// document.querySelector("#expand-arrow").onchange = function (e) {
-//   const previousWeatherElement = document.querySelector("#previous-weather");
-//   previousWeatherElement.classList.toggle("show-weather");
-// };
+//Show previous weather
+document.querySelector("#show-previous-weather").onclick = function (e) {
+  const previousWeatherElement = document.querySelector("#previous-weather");
+  previousWeatherElement.classList.toggle("show-weather");
+};
